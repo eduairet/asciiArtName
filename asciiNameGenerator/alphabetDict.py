f40485050c5ba8e7cbda525c9f80863bbda8d43a
eduairet <hola@eduairet.com>
2021-06-10 01:30:45 -0500

    Program finished
    Program functional

--- asciiNameGenerator/alphabetDict.py
+++ asciiNameGenerator/alphabetDict.py
@@ -1,52 +1,81 @@
+from random import choice
 # Define your letters list from a string
-letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-letters = letters.replace('', ' ')
-lettersList = letters.split()
+letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ '
+letters = letters.replace('', '\r')
+lettersList = [letter for letter in letters.split('\r') if letter != '']
+# Create a character class to hold the character and its methods
+class Character:
+    def __init__(self, name):
+        self.name = name
+        self.draw = []
+    def drawCharacter(self):
+        for i, row in enumerate(self.draw):
+            for j, character in enumerate(row):
+                if character:
+                    self.draw[i][j] = self.name
+                else:
+                    self.draw[i][j] = ' '
+    def randomizeDraw(self):
+        randomDraw = self.draw
+        for i, row in enumerate(randomDraw):
+            for j, character in enumerate(row):
+                if character != ' ':
+                    randomDraw[i][j] = choice(lettersList[:-2])
+        return randomDraw
 # Create a dict with every letter
 lettersDict = {}
 # Fill the letters dictionary
 for letter in lettersList:
-    lettersDict[letter] = [] # This empty list will be populated later with the letters
+    lettersDict[letter] = Character(letter) # This empty list will be populated later with the letters
 # Variables for drawing, names are in order to get better contrast
 W, _ = True, False
 # Here all the letters are drawn
-lettersDict['A'] = [
-    [_,W,W,W,_],
-    [W,_,_,W,W],
-    [W,_,_,W,W],
-    [W,_,_,W,W],
-    [W,W,W,W,W],
-    [W,_,_,W,W],
-    [W,_,_,W,W]
+lettersDict[' '].draw = [
+    [_,_,_,_],
+    [_,_,_,_],
+    [_,_,_,_],
+    [_,_,_,_],
+    [_,_,_,_],
+    [_,_,_,_],
+    [_,_,_,_]
 ]
-lettersDict['B'] = [
-    [W,W,W,W,_],
-    [W,W,_,_,W],
-    [W,W,_,_,W],
-    [W,W,W,W,_],
-    [W,W,_,_,W],
-    [W,W,_,_,W],
-    [W,W,W,W,_]
+lettersDict['A'].draw = [
+    [_,W,W,W,W,_],
+    [W,W,_,_,W,W],
+    [W,W,_,_,W,W],
+    [W,W,_,_,W,W],
+    [W,W,W,W,W,W],
+    [W,W,_,_,W,W],
+    [W,W,_,_,W,W]
 ]
-lettersDict['C'] = [
-    [_,W,W,W,_],
-    [W,W,_,_,W],
-    [W,W,_,_,_],
-    [W,W,_,_,_],
-    [W,W,_,_,_],
-    [W,W,_,_,W],
-    [_,W,W,W,_]
+lettersDict['B'].draw = [
+    [W,W,W,W,W,_],
+    [W,W,_,_,W,W],
+    [W,W,_,_,W,W],
+    [W,W,W,W,W,_],
+    [W,W,_,_,W,W],
+    [W,W,_,_,W,W],
+    [W,W,W,W,W,_]
 ]
-lettersDict['D'] = [
-    [W,W,W,W,_],
-    [W,W,_,_,W],
-    [W,W,_,_,W],
-    [W,W,_,_,W],
-    [W,W,_,_,W],
-    [W,W,_,_,W],
-    [W,W,W,W,_]
+lettersDict['C'].draw = [
+    [_,W,W,W,W,_],
+    [W,W,_,_,W,W],
+    [W,W,_,_,_,_],
+    [W,W,_,_,_,_],
+    [W,W,_,_,_,_],
+    [W,W,_,_,W,W],
+    [_,W,W,W,W,_]
 ]
-lettersDict['E'] = [
+lettersDict['D'].draw = [
+    [W,W,W,W,W,_],
+    [W,W,_,_,W,W],
+    [W,W,_,_,W,W],
+    [W,W,_,_,W,W],
+    [W,W,_,_,W,W],
+    [W,W,_,_,W,W],
+    [W,W,W,W,W,_]
+]
+lettersDict['E'].draw = [
     [W,W,W,W,W],
     [W,W,_,_,_],
     [W,W,_,_,_],
@@ -55,7 +84,7 @@ lettersDict['E'] = [
     [W,W,_,_,_],
     [W,W,W,W,W]
 ]
-lettersDict['F'] = [
+lettersDict['F'].draw = [
     [W,W,W,W,W],
     [W,W,_,_,_],
     [W,W,_,_,_],
@@ -64,25 +93,25 @@ lettersDict['F'] = [
     [W,W,_,_,_],
     [W,W,_,_,_]
 ]
-lettersDict['G'] = [
-    [_,W,W,W,_],
-    [W,W,_,_,W],
-    [W,W,_,_,_],
-    [W,W,_,W,W],
-    [W,W,_,_,W],
-    [W,W,_,_,W],
-    [_,W,W,W,_]
+lettersDict['G'].draw = [
+    [_,W,W,W,W,_],
+    [W,W,_,_,W,W],
+    [W,W,_,_,_,_],
+    [W,W,_,W,W,W],
+    [W,W,_,_,W,W],
+    [W,W,_,_,W,W],
+    [_,W,W,W,W,_]
 ]
-lettersDict['H'] = [
-    [W,W,_,_,W],
-    [W,W,_,_,W],
-    [W,W,_,_,W],
-    [W,W,W,W,W],
-    [W,W,_,_,W],
-    [W,W,_,_,W],
-    [W,W,_,_,W]
+lettersDict['H'].draw = [
+    [W,W,_,_,W,W],
+    [W,W,_,_,W,W],
+    [W,W,_,_,W,W],
+    [W,W,W,W,W,W],
+    [W,W,_,_,W,W],
+    [W,W,_,_,W,W],
+    [W,W,_,_,W,W]
 ]
-lettersDict['I'] = [
+lettersDict['I'].draw = [
     [W,W],
     [W,W],
     [W,W],
@@ -91,25 +120,25 @@ lettersDict['I'] = [
     [W,W],
     [W,W]
 ]
-lettersDict['J'] = [
-    [_,_,W,W],
-    [_,_,W,W],
-    [_,_,W,W],
-    [_,_,W,W],
-    [W,_,W,W],
-    [W,_,W,W],
-    [_,W,W,_]
+lettersDict['J'].draw = [
+    [_,_,_,W,W],
+    [_,_,_,W,W],
+    [_,_,_,W,W],
+    [_,_,_,W,W],
+    [W,W,_,W,W],
+    [W,W,_,W,W],
+    [_,W,W,W,_]
 ]
-lettersDict['K'] = [
-    [W,W,_,_,W],
-    [W,W,_,_,W],
-    [W,W,_,W,_],
-    [W,W,W,_,_],
-    [W,W,_,W,_],
-    [W,W,_,_,W],
-    [W,W,_,_,W]
+lettersDict['K'].draw = [
+    [W,W,_,_,W,W],
+    [W,W,_,_,W,W],
+    [W,W,_,W,W,_],
+    [W,W,W,W,_,_],
+    [W,W,_,W,W,_],
+    [W,W,_,_,W,W],
+    [W,W,_,_,W,W]
 ]
-lettersDict['L'] = [
+lettersDict['L'].draw = [
     [W,W,_,_],
     [W,W,_,_],
     [W,W,_,_],
@@ -118,52 +147,61 @@ lettersDict['L'] = [
     [W,W,_,_],
     [W,W,W,W]
 ]
-lettersDict['M'] = [
-    [W,W,_,_,_,W,W],
-    [W,W,_,_,W,W,W],
-    [W,W,W,_,W,W,W],
-    [W,_,W,W,_,W,W],
-    [W,_,W,W,_,W,W],
-    [W,_,_,_,_,W,W],
-    [W,_,_,_,_,W,W]
+lettersDict['M'].draw = [
+    [W,W,_,_,_,_,W,W],
+    [W,W,W,_,_,W,W,W],
+    [W,W,W,_,_,W,W,W],
+    [W,W,W,W,W,W,W,W],
+    [W,W,_,W,W,_,W,W],
+    [W,W,_,W,W,_,W,W],
+    [W,W,_,_,_,_,W,W]
 ]
-lettersDict['N'] = [
-    [W,W,_,_,W],
-    [W,W,_,_,W],
-    [W,W,W,_,W],
-    [W,W,W,_,W],
-    [W,_,W,W,W],
-    [W,_,_,W,W],
-    [W,_,_,W,W]
+lettersDict['N'].draw = [
+    [W,W,_,_,_,W],
+    [W,W,W,_,_,W],
+    [W,W,W,W,_,W],
+    [W,_,W,W,_,W],
+    [W,_,W,W,W,W],
+    [W,_,_,W,W,W],
+    [W,_,_,_,W,W]
 ]
-lettersDict['O'] = [
-    [_,W,W,W,_],
-    [W,W,_,_,W],
-    [W,W,_,_,W],
-    [W,W,_,_,W],
-    [W,W,_,_,W],
-    [W,W,_,_,W],
-    [_,W,W,W,_]
+lettersDict['O'].draw = [
+    [_,W,W,W,W,_],
+    [W,W,_,_,W,W],
+    [W,W,_,_,W,W],
+    [W,W,_,_,W,W],
+    [W,W,_,_,W,W],
+    [W,W,_,_,W,W],
+    [_,W,W,W,W,_]
 ]
-lettersDict['P'] = [
-    [W,W,W,W,_],
-    [W,W,_,_,W],
-    [W,W,_,_,W],
-    [W,W,_,_,W],
-    [W,W,W,W,_],
-    [W,W,_,_,_],
-    [W,W,_,_,_]
+lettersDict['P'].draw = [
+    [W,W,W,W,W,_],
+    [W,W,_,_,W,W],
+    [W,W,_,_,W,W],
+    [W,W,_,_,W,W],
+    [W,W,W,W,W,_],
+    [W,W,_,_,_,_],
+    [W,W,_,_,_,_]
 ]
-lettersDict['Q'] = [
-    [_,W,W,W,_,_],
-    [W,W,_,_,W,_],
-    [W,W,_,_,W,_],
-    [W,W,_,_,W,_],
-    [W,W,_,_,W,_],
-    [W,W,_,_,W,_],
-    [_,W,W,W,W,W]
+lettersDict['Q'].draw = [
+    [_,W,W,W,W,_,_],
+    [W,W,_,_,W,W,_],
+    [W,W,_,_,W,W,_],
+    [W,W,_,_,W,W,_],
+    [W,W,_,_,W,W,_],
+    [W,W,_,_,W,W,_],
+    [_,W,W,W,W,W,W]
 ]
-lettersDict['S'] = [
+lettersDict['R'].draw = [
+    [W,W,W,W,W,_],
+    [W,W,_,_,W,W],
+    [W,W,_,_,W,W],
+    [W,W,_,_,W,W],
+    [W,W,W,W,W,_],
+    [W,W,_,_,W,W],
+    [W,W,_,_,W,W]
+]
+lettersDict['S'].draw = [
     [_,W,W,W,_],
     [W,W,_,_,W],
     [W,W,W,_,_],
@@ -172,7 +210,7 @@ lettersDict['S'] = [
     [W,_,_,W,W],
     [_,W,W,W,_]
 ]
-lettersDict['T'] = [
+lettersDict['T'].draw = [
     [W,W,W,W,W,W],
     [_,_,W,W,_,_],
     [_,_,W,W,_,_],
@@ -181,66 +219,60 @@ lettersDict['T'] = [
     [_,_,W,W,_,_],
     [_,_,W,W,_,_]
 ]
-lettersDict['U'] = [
-    [W,W,_,_,W],
-    [W,W,_,_,W],
-    [W,W,_,_,W],
-    [W,W,_,_,W],
-    [W,W,_,_,W],
-    [W,W,_,_,W],
-    [_,W,W,W,_]
+lettersDict['U'].draw = [
+    [W,W,_,_,W,W],
+    [W,W,_,_,W,W],
+    [W,W,_,_,W,W],
+    [W,W,_,_,W,W],
+    [W,W,_,_,W,W],
+    [W,W,_,_,W,W],
+    [_,W,W,W,W,_]
 ]
-lettersDict['V'] = [
-    [W,W,_,_,W],
-    [W,W,_,_,W],
-    [W,W,_,_,W],
-    [W,W,_,_,W],
-    [W,W,_,_,W],
-    [_,W,W,W,_],
-    [_,_,W,_,_]
+lettersDict['V'].draw = [
+    [W,W,_,_,W,W],
+    [W,W,_,_,W,W],
+    [W,W,_,_,W,W],
+    [W,W,_,_,W,W],
+    [W,W,_,_,W,W],
+    [_,W,W,W,W,_],
+    [_,_,W,W,_,_]
 ]
-lettersDict['W'] = [
-    [W,W,_,W,W,_,W],
-    [W,W,_,W,W,_,W],
-    [W,W,_,W,W,_,W],
-    [W,W,_,W,W,_,W],
-    [W,W,_,W,W,_,W],
-    [_,W,W,W,W,W,W],
-    [_,_,W,_,_,W,_]
+lettersDict['W'].draw = [
+    [W,W,_,W,W,_,W,W],
+    [W,W,_,W,W,_,W,W],
+    [W,W,_,W,W,_,W,W],
+    [W,W,_,W,W,_,W,W],
+    [W,W,_,W,W,_,W,W],
+    [_,W,W,W,W,W,W,_],
+    [_,_,W,_,_,W,_,_]
 ]
-lettersDict['X'] = [
-    [W,W,_,_,W],
-    [W,W,_,_,W],
-    [W,W,_,W,_],
-    [_,W,W,W,_],
-    [_,W,W,W,W],
-    [W,_,_,W,W],
-    [W,_,_,W,W]
-]
-lettersDict['Y'] = [
+lettersDict['X'].draw = [
     [W,W,_,_,_,W],
     [W,W,_,_,_,W],
-    [W,W,_,_,_,W],
-    [_,W,W,_,W,_],
+    [W,W,W,_,W,_],
+    [_,_,W,W,_,_],
+    [_,W,_,W,W,W],
+    [W,_,_,_,W,W],
+    [W,_,_,_,W,W]
+]
+lettersDict['Y'].draw = [
+    [W,W,_,_,W,W],
+    [W,W,_,_,W,W],
+    [W,W,_,_,W,W],
+    [_,W,W,W,W,_],
     [_,_,W,W,_,_],
     [_,_,W,W,_,_],
     [_,_,W,W,_,_]
 ]
-lettersDict['Z'] = [
-    [W,W,W,W,W],
-    [_,_,_,W,W],
-    [_,_,_,W,W],
-    [_,_,W,W,_],
-    [_,W,W,_,_],
-    [W,W,_,_,_],
-    [W,W,W,W,W]
+lettersDict['Z'].draw = [
+    [W,W,W,W,W,W],
+    [_,_,_,_,W,W],
+    [_,_,_,W,W,W],
+    [_,W,W,W,W,_],
+    [W,W,W,_,_,_],
+    [W,W,_,_,_,_],
+    [W,W,W,W,W,W]
 ]
 # Replace True values with the letter of the character
-for key, value in lettersDict.items():
-    for i, row in enumerate(value):
-        for j, character in enumerate(row):
-            if character:
-                lettersDict[key][i][j] = key
-            else:
-                lettersDict[key][i][j] = ' '
-
+for letter in lettersDict.values():
+    letter.drawCharacter()
